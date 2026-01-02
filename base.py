7658430adf596e6f64b3cddbb5db2ad93b1bcc9a
Ava <phaidyme@protonmail.com>
2026-01-03 10:22:01 +1100

    idk

--- base.py
+++ base.py
@@ -1,19 +1,29 @@
 import random
 
-random.seed(0)  # want the rolls to be the same each time
+random.seed(0) # want the rolls to be the same each time
 
 def print_dict(dictionary, level=0):
-    dictionary = {
-        key: dictionary[key] for key in sorted(list(dictionary.keys()))
-    }
-    for key, value in dictionary.items():
+    keys = sorted(list(dictionary.keys()))
+    nested_keys = []
+    numbered_keys = []
+    single_keys = []
+    for key in keys:
+        if type(dictionary[key]) is dict:
+            nested_keys.append(key)
+        elif dictionary[key] > 1:
+            numbered_keys.append(key)
+        else:
+            single_keys.append(key)
+    keys = single_keys + numbered_keys + nested_keys
+    for key in keys:
+        value = dictionary[key]
         if type(value) is dict:
-            print(level * "  ", key)
+            print(level * "  " + key)
             print_dict(value, level + 1)
         elif value == 1:
-            print(level * "  ", key)
+            print(level * "  " + key)
         else:
-            print(level * "  ", value, "x", key)
+            print(level * "  " + str(value), "x", key)
 
 def D(n, advantage=False, disadvantage=False):
     if advantage:
@@ -32,8 +42,44 @@ class Armour:
     def __init__(self, name, base_ac):
         self.name = name
         self.base_ac = base_ac
-        
-        
+
+class Skill:
+    def __init__(self, name, ability):
+        self.name = name
+        self.ability = ability
+    def __str__(self):
+        return self.name    
+
+initiative = Skill("initiative", "dexterity")
+
+athletics = Skill("athletics", "strength")
+
+acrobatics = Skill("acrobatics", "dexterity")
+sleight_of_hand = Skill("sleight_of_hand", "dexterity")
+stealth = Skill("stealth", "dexterity")
+
+arcana = Skill("arcana", "intelligence")
+history = Skill("history", "intelligence")
+investigation = Skill("investigation", "intelligence")
+nature = Skill("nature", "intelligence")
+religion = Skill("religion", "intelligence")
+
+animal_handling = Skill("animal_handling", "wisdom")
+insight = Skill("insight", "wisdom")
+medicine = Skill("medicine", "wisdom")
+perception = Skill("perception", "wisdom")
+survival = Skill("survival", "wisdom")
+
+deception = Skill("deception", "charisma")
+intimidation = Skill("intimidation", "charisma")
+performance = Skill("performance", "charisma")
+persuasion = Skill("persuasion", "charisma")
+
+strength_save = Skill("strength save", "strength")
+constitution_save = Skill("constitution save", "constitution")
+
+def mod(ability_score):
+    return int((ability_score - 10) / 2)
 class Character:
     def __init__(self, strength, dexterity, constitution, intelligence, wisdom, charisma):
         self.level = 1
@@ -46,67 +92,9 @@ class Character:
         self.wisdom = wisdom
         self.charisma = charisma
 
-        self.skills = {
-            "initiative": {
-                "ability": "dexterity", "proficiency": 0
-            },
-            "acrobatics": {
-                "ability": "dexterity", "proficiency": 0
-            },
-            "animal handling": {
-                "ability": "wisdom", "proficiency": 0
-            },
-            "arcana": {
-                "ability": "intelligence", "proficiency": 0
-            },
-            "athletics": {
-                "ability": "strength", "proficiency": 0
-            },
-            "deception": {
-                "ability": "charisma", "proficiency": 0
-            },
-            "history": {
-                "ability": "intelligence", "proficiency": 0
-            },
-            "insight": {
-                "ability": "wisdom", "proficiency": 0
-            },
-            "intimidation": {
-                "ability": "charisma", "proficiency": 0
-            },
-            "investigation": {
-                "ability": "intelligence", "proficiency": 0
-            },
-            "medicine": {
-                "ability": "wisdom", "proficiency": 0
-            },
-            "nature": {
-                "ability": "intelligence", "proficiency": 0
-            },
-            "perception": {
-                "ability": "wisdom", "proficiency": 0
-            },
-            "performance": {
-                "ability": "charisma", "proficiency": 0
-            },
-            "persuasion": {
-                "ability": "charisma", "proficiency": 0
-            },
-            "religion": {
-                "ability": "intelligence", "proficiency": 0
-            },
-            "slight of hand": {
-                "ability": "dexterity", "proficiency": 0
-            },
-            "stealth": {
-                "ability": "dexterity", "proficiency": 0
-            },
-            "survival": {
-                "ability": "wisdom", "proficiency": 0
-            },
-        }
+        self.proficiencies = {}
     def get_ac(self):
-        return self.armour.base_ac + self.get_modifier("dexterity")
+        return self.armour.base_ac + mod(self.dexterity)
 
     def attack(self, weapon, advantage = False, disadvantage = False):
         print(f" - attacking with {weapon.name}")
@@ -118,7 +106,7 @@ class Character:
             return
         
         damage = sum([
-            self.get_modifier("dexterity"),
+            mod(self.dexterity),
             self.proficiency_bonus,
             sum([D(die) for die in weapon.dice]),
             weapon.plus
@@ -127,7 +115,7 @@ class Character:
         if attack_roll == 20:
             print("   critical hit!")
             critical = sum([
-                self.get_modifier("dexterity"),
+                mod(self.dexterity),
                 self.proficiency_bonus,
                 sum(weapon.dice),
                 weapon.plus
@@ -138,7 +126,7 @@ class Character:
                 f"   rolled {attack_roll},",
                 (
                     attack_roll
-                    + self.get_modifier('dexterity')
+                    + mod(self.dexterity)
                     + self.proficiency_bonus
                     + weapon.plus
                 ),
@@ -146,27 +134,63 @@ class Character:
             )
             print(f"   {damage} damage")
         return attack_roll
-    def get_modifier(self, ability):
-        return int((getattr(self, ability) - 10) / 2)
-    def roll(skill, advantage = False, disadvantage = False):
+    def roll(self, skill, advantage = False, disadvantage = False):
         n = D(20, advantage, disadvantage)
-        modifier = self.get_modifier(self.skills[skill]["ability"])
-        bonus = self.proficiency_bonus + self.skills[skill]["proficiency"]
-        result = n + modifier + bonus
-        print(f"rolled {result} for {skill} ({n} + {modifier} + {bonus})")
+        modifier = mod(getattr(self, skill.ability))
+        proficiency = self.proficiency_bonus * self.proficiencies.get(
+            skill.name,
+            0 # default to zero if don't have proficiency in skill
+        )
+        result = n + modifier + proficiency
+        print(f"rolled {result} for {skill} ({n} + {modifier} + {proficiency})")
         return result
     def long_rest(self):
         self.curr_hp = self.full_hp
         self.hit_dice = self.level
+        self.SuperiorityDice = self.mSuperiorityDice
     def short_rest(self):
+        self.SuperiorityDice = self.mSuperiorityDice
         print(" - short rest")
         i = self.hit_dice
         n = 0
         total_healed = 0
         while(self.hit_dice > 0 and self.curr_hp + 10 < self.full_hp):
-            healing = D(10) + self.get_modifier("constitution")
+            healing = D(10) + mod(self.constitution)
             self.hit_dice -= 1
             self.curr_hp += healing
             n += 1
             total_healed += healing
         print(f"   used {n}/{i} hit dice to heal {total_healed} HP")
+
+if __name__ == "__main__":
+    n = int(1e5)
+
+    print("flat D20 (should be 10.5)")
+    print(sum([D(20) for i in range(n)]) / n)
+    
+    print("D20 with advantage (should be 13.825)")
+    print(sum([D(20, advantage=True) for i in range(n)]) / n)
+
+    print("D20 with disadvantage (should be 7.175)")
+    print(sum([D(20, disadvantage=True) for i in range(n)]) / n)
+
+    print(sum([-205,
+        45, # studded leather
+        10, # shield
+        25, # rapier
+        50, # longbow
+        1, # arrows
+        2, # backpack
+        1, # bedroll
+        5, # chain
+        2, # traveler's clothes
+        2, # crowbar
+        2, # grappling hook
+        0.02, # mess kit
+        1, # quiver
+        1, # rations
+        1, # rope
+        0.02, # soap
+        0.2, # waterskin
+        0.01, # whetstone
+    ]))
--- karla.py
+++ karla.py
@@ -1,32 +1,49 @@
+"""
+  - tiefling (fiendish legacy)
+  - family is working class but rich enough to hang out with nobility
+  - family tries really hard to belong in that circle
+    including hiding the racialised aspects of their appearance and demeanour
+
+  - raised and trained from birth to become high ranked city guard
+  - possibly had a rivalry/friendship with Ilya and felt jealousy towards her
+    because of internalised racism
+  - volunteered to track Ilya down, half because she was mad that Ilya dared
+    abandon the life goal she thought they shared, half because she wanted
+    to bring her friend back
+
+  - dex based battle master fighter
+  - duelling fighting style, "rapier" and shield
+    rapier not bendy, more like a thin and light longsword
+"""
 from base import *
 
-karla = Character(10, 15, 14, 14, 10, 8)
-# custom tiefling
+karla = Character(8, 15, 15, 10, 14, 8)
+# fiendish legacy tiefling
+karla.spells = ["fire bolt"]
+# fighter
+karla.proficiencies["perception"] = 1
+karla.proficiencies["acrobatics"] = 1
+# soldier background
+karla.proficiencies["athletics"] = 1
+karla.proficiencies["intimidation"] = 1
 karla.dexterity += 2
 karla.constitution += 1
-# two proficiencies from background (noble)
-karla.skills["history"]["proficiency"] = 1
-karla.skills["persuasion"]["proficiency"] = 1
-# three proficiencies from origin feat (skilled)
-karla.skills["acrobatics"]["proficiency"] = 1
-karla.skills["investigation"]["proficiency"] = 1
-karla.skills["insight"]["proficiency"] = 1
+# martial adept as origin feat
+karla.mSuperiorityDice = [6]
+karla.maneuvers = ["riposte", "trip"]
 # level 1 bonus ASI - thank you Kaia! <3
-karla.dexterity += 1
-karla.constitution += 1
+karla.dexterity += 2
 # fighter hit die is D10
-karla.full_hp = 10 + karla.get_modifier("constitution")
-# starting gear is custom
-karla.gold = 10
-karla.silver = 0
-karla.copper = 0
+karla.full_hp = 10 + mod(karla.constitution)
+karla.gold = 56
+karla.silver = 7
+karla.copper = 5
 karla.inventory = {
     "quiver": {
-        "arrow": 50,
+        "arrow": 20,
     },
     "backpack": {
         "bedroll": 1,
-        "blanket": 1,
         "mess kit": {
             "cup": 1,
             "simple cutlery": 1,
@@ -36,12 +53,14 @@ karla.inventory = {
         "waterskin": 1,
         "rations": 10,
         "hempen rope": 50,
-        "chain": 10,
         "soap": 1,
         "whetstone": 1,
-    }
+        "crowbar": 1
+    },
+    "chain": 10,
+    "grappling hook": 1,
 }
-karla.clothes = "common clothes"
+karla.clothes = "traveler's  clothes"
 karla.armour = Armour("studded leather", 12)
 karla.shield = Armour("shield", 2)
 
@@ -55,35 +74,35 @@ karla.full_hp += 6 + 3
 # level 3
 karla.level = 3
 karla.full_hp += 6 + 3
-karla.superiority_dice = 4
-karla.maneuvers = [
+# from battlemaster fighter
+karla.mSuperiorityDice += [8, 8, 8, 8]
+karla.maneuvers += [
+    "distracting strike",
     "commander's strike",
-    "riposte",
-    "trip attack"
+    "brace"
 ]
-# one artisan's tools and one fighter skill proficiency
-karla.skills["perception"]["proficiency"] = 1
-karla.skills["smith`s tools"] = {
-    "ability": "strength", "proficiency": 1
-}
+karla.proficiencies["history"] = 1
+karla.proficiencies["woodcarver's tools"] = 1
+# from fiendish legacy
+karla.spells.append("hellish rebuke")
 
 # level 4
 karla.level = 4
 karla.full_hp += 6 + 3
-# ASI
-karla.dexterity += 2
+# skill expert feat
+karla.dexterity += 1
+karla.proficiencies["insight"] = 1
+karla.proficiencies["perception"] = 2
 
 karla.long_rest()
+karla.roll(perception)
 
-print("----- inventory -----")
+print(" inventory ".center(80, '-'))
 print_dict(karla.inventory)
-print("---------------------")
-print('',
-    karla.gold, "gold,",
-    karla.silver, "silver,",
-    karla.copper, "copper"
-)
-print(f" {karla.curr_hp} / {karla.full_hp} HP, {karla.hit_dice} hit die")
-print(f" {karla.get_ac()} AC + {karla.shield.base_ac} with shield")
-print(f" {karla.superiority_dice} superiority dice")
-print(" maneuvers:", ', '.join(karla.maneuvers))
+print(" status ".center(80, '-'))
+print(f"{karla.gold} gold, {karla.silver} silver, {karla.copper} copper")
+print(f"{karla.curr_hp} / {karla.full_hp} HP, {karla.hit_dice} hit die")
+print(f"{karla.get_ac()} AC + {karla.shield.base_ac} with shield")
+print("spells:", ', '.join(karla.spells), "(once per long rest)")
+print("maneuvers:", ', '.join(karla.maneuvers))
+print("superiority dice:", *(karla.SuperiorityDice))
